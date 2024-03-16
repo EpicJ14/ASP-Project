21398a99bfc90317ea8c43cc47fe310b8495d7ad
Kaloyan Kolev <124430813+EpicJ14@users.noreply.github.com>
2024-03-16 10:19:21 +0200

    Add files via upload
    Initial upload

---
+++ Models/DbServicesContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Linq;
+using System.Data.Entity;
+
+namespace BankSys.Models
+{
+    public class DbServicesContext : DbContext
+    {
+        public DbSet<User> users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankSys.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
