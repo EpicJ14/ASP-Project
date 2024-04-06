848e91e909535a4b0ab55222725615f75b44d328
Kaloyan Kolev <124430813+EpicJ14@users.noreply.github.com>
2024-04-06 23:16:55 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BankSys.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
