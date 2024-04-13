7faa5c3175587b50e3a34b06c1bbedcda7431ab9
Kaloyan Kolev <124430813+EpicJ14@users.noreply.github.com>
2024-04-13 10:41:30 +0300

    Delete Models/BankAccount.cs

--- Models/BankAccount.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bank4.Models
-{
-    public class BankAccount
-    {
-        public int id { get; set; }
-        public int user_id { get; set; }
-        public double Balance { get; set; }
-    }
-}
